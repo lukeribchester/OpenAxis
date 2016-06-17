d360c11b3a00e741535423ac8d4ae525ab4ce385
Luke Ribchester <lukeribchester@users.noreply.github.com>
2016-06-17 04:44:46 +0100

    Implement Passport with local authentication

--- app.js
+++ app.js
@@ -6,9 +6,53 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var db = require('./db');
 
 var routes = require('./routes/index');
 
+// connect to mongodb database
+var url = 'mongodb://localhost:27017/openaxis_test';
+MongoClient.connect(url, function (err, db) {
+    assert.equal(null, err);
+    console.log("Connected to MongoDB server.");
+    db.close();
+});
+
+// Passport configuration
+// configure passport-local strategy
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        db.users.findByUsername(username, function (err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false, {message: 'Incorrect username.'});
+            }
+            if (user.password != password) {
+                return done(null, false, {message: 'Incorrect password.'});
+            }
+            return done(null, user);
+        });
+    }
+));
+
+// configure authenticated session persistence
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    db.users.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+// create express application
 var app = express();
 
 // register partials
@@ -18,22 +62,35 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(logger('dev'));
+app.use(require('morgan')('combined'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(require('express-session')({ secret: 'secret', resave: false, saveUninitialized: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/public', express.static(__dirname + '/public'));
+
+// express-messages and connect-flash middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
+// initialise passport, restore session authentication state
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -41,23 +98,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,61 @@
 var express = require('express');
 var router = express.Router();
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-
-var url = 'mongodb://localhost:27017/openaxis_test';
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected to MongoDB server.");
-    db.close();
-});
+var passport = require('passport');
+var connectEnsureLogin = require('connect-ensure-login');
 
 /* GET root page. */
-router.get('/', function (req, res, next) {
-    res.render('./pages/live', {title: 'Live'});
+router.get('/', connectEnsureLogin.ensureLoggedIn('/login'),
+    function (req, res, next) {
+    res.render('./pages/live', {title: 'Live', user: req.user});
 });
 
 /* GET live page. */
-router.get('/live', function (req, res, next) {
-    res.render('./pages/live', {title: 'Live'});
+router.get('/live', connectEnsureLogin.ensureLoggedIn('/login'),
+    function (req, res, next) {
+    res.render('./pages/live', {title: 'Live', user: req.user});
 });
 
 /* GET events page. */
-router.get('/events', function (req, res, next) {
-    res.render('./pages/events', {title: 'Events'});
+router.get('/events', connectEnsureLogin.ensureLoggedIn('/login'),
+    function (req, res, next) {
+    res.render('./pages/events', {title: 'Events', user: req.user});
 });
 
 /* GET archive page. */
-router.get('/archive', function (req, res, next) {
-    res.render('./pages/archive', {title: 'Archive'});
+router.get('/archive', connectEnsureLogin.ensureLoggedIn('/login'),
+    function (req, res, next) {
+    res.render('./pages/archive', {title: 'Archive', user: req.user});
+});
+
+/* GET settings page. */
+router.get('/settings', connectEnsureLogin.ensureLoggedIn('/login'),
+    function (req, res, next) {
+    res.render('./pages/settings', {title: 'Settings', user: req.user});
+});
+
+/* GET login page. */
+router.get('/login', function (req, res, next) {
+    res.render('./pages/login', {title: 'Sign in', user: req.user});
+});
+
+/* POST login page. */
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}));
+
+
+/* GET logout page. */
+router.get('/logout', connectEnsureLogin.ensureLoggedIn('/login'),
+    function (req, res, next) {
+    req.logout();
+    res.redirect('/login');
+});
+
+/* GET test page. */
+router.get('/test', connectEnsureLogin.ensureLoggedIn('/login'),
+    function (req, res, next) {
+    res.render('./pages/test', {title: 'Test', user: req.user});
 });
 
 module.exports = router;
